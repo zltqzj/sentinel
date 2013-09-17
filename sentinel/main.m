a154e5f7fa24ad275ca62ced09abe115c5a4b915
zltqzj <zltqzj@163.com>
2013-09-17 16:50:01 +0800

    Initial Commit

---
+++ sentinel/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  sentinel
+//
+//  Created by Sinosoft on 9/17/13.
+//  Copyright (c) 2013 com.Sinosoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
