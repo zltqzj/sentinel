8690e022f44c034b5503ad4e09f34f654f2a563f
zltqzj <zltqzj@163.com>
2013-09-24 10:00:10 +0800

    sentinel
    可变参数（C语言）

--- sentinel/main.m
+++ sentinel/main.m
@@ -7,14 +7,64 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <stdarg.h>
+
+void printStrings(char *first,...) __attribute__((sentinel));
+
+void printString(char *first,...){
+    va_list args;
+    va_start(args, first);
+    char *string = first;
+    while (string != NULL) {
+        printf("%s",string);
+        string = va_arg(args, char *);
+    }
+    va_end(args);
+    printf("\n");
+}
+
+
+
+
+int addemUp(int firstNum,...){
+    va_list args;
+    int sum = firstNum;
+    int number;
+    
+    va_start(args, firstNum);
+    while (1) {
+        number = va_arg(args, int);
+        sum += number;
+        if (number == 0) {
+            break;
+        }
+    }
+    va_end(args);
+    return sum;
+    
+}
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        char* str = "";
+        if (str == NULL) {
+            NSLog(@"dashi");
+        }
+        else
+        {
+            NSLog(@"aaa");
+        }
+        
+        int sumbody = addemUp(1,2,3,4,5,6,7,8,9,0);
+        
+        printf("sum of 1-9 is %d\n",sumbody);
+        
+//        printStrings("spicy","pony","head", NULL);
+//        
+//        printString("machine","tool");
         
     }
     return 0;
